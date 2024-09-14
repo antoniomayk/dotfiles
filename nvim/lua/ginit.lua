ce437d2e727852f541903ceb0128706c546a35da
antoniomayk <antoniomayk.g@gmail.com>
2024-09-14 11:30:50 -0300

    feat: change font name and size

--- nvim/lua/ginit.lua
+++ nvim/lua/ginit.lua
@@ -8,9 +8,10 @@ vim.g.neovide_cursor_animation_length = 0.13
 vim.g.neovide_touch_drag_timeout = 0.20
 vim.g.remember_window_size = true
 vim.g.remember_window_position = true
-vim.g.gui_font_default_size = 13
+vim.g.gui_font_default_size = 15
 vim.g.gui_font_size = vim.g.gui_font_default_size
-vim.g.gui_font_face = "JetBrainsMono NF"
+vim.g.gui_font_face = "CaskaydiaCove NF"
+vim.opt.linespace = 0
 
 -- utils
 --
@@ -24,7 +25,7 @@ local function toggle_fullscreen()
 end
 
 local function refresh_gui_font()
-  vim.o.guifont = string.format("%s:h%s", vim.g.gui_font_face, vim.g.gui_font_size)
+  vim.o.guifont = string.format("%s:h%s:w-0.4:#e-subpixelantialias:#h-normal", vim.g.gui_font_face, vim.g.gui_font_size)
 end
 
 local function resize_gui_font(delta)
