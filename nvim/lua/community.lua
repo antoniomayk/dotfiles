827a56eac16acd1a367212030d84c5ffe282f5ee
antoniomayk <antoniomayk.g@gmail.com>
2024-10-02 23:49:51 -0300

    feat: improve astronvim utilization

--- nvim/lua/community.lua
+++ nvim/lua/community.lua
@@ -11,6 +11,8 @@ return {
   { import = "astrocommunity.color.nvim-highlight-colors" },
   -- colorscheme
   { import = "astrocommunity.colorscheme.catppuccin" },
+  -- colorscheme
+  { import = "astrocommunity.code-runner.overseer-nvim" },
   -- debugging
   { import = "astrocommunity.debugging.nvim-dap-virtual-text" },
   { import = "astrocommunity.debugging.nvim-dap-repl-highlights" },
@@ -18,7 +20,9 @@ return {
   -- diagnostics
   -- { import = "astrocommunity.diagnostics.lsp_lines-nvim" },
   { import = "astrocommunity.diagnostics.trouble-nvim" },
+  -- editing-support
   { import = "astrocommunity.editing-support.conform-nvim" },
+  -- { import = "astrocommunity.editing-support.auto-save-nvim" },
   -- keybindings
   { import = "astrocommunity.keybinding.nvcheatsheet-nvim" },
   -- pack
@@ -26,6 +30,7 @@ return {
   { import = "astrocommunity.pack.fish" },
   { import = "astrocommunity.pack.haskell" },
   { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.cmake" },
   { import = "astrocommunity.pack.cpp" },
   { import = "astrocommunity.pack.angular" },
   { import = "astrocommunity.pack.dart" },
@@ -33,8 +38,13 @@ return {
   { import = "astrocommunity.pack.toml" },
   { import = "astrocommunity.pack.java" },
   { import = "astrocommunity.pack.scala" },
-  -- { import = "astrocommunity.pack.kotlin" }, Doesn't work well!
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.python-ruff" },
+  { import = "astrocommunity.pack.typescript-all-in-one" },
+  -- -- { import = "astrocommunity.pack.kotlin" }, Doesn't work well!
   { import = "astrocommunity.pack.rainbow-delimiter-indent-blankline" },
+  -- lsp
+  { import = "astrocommunity.project.projectmgr-nvim" },
   -- lsp
   { import = "astrocommunity.lsp.delimited-nvim" },
   { import = "astrocommunity.lsp.actions-preview-nvim" },
@@ -43,6 +53,14 @@ return {
   { import = "astrocommunity.lsp.lsp-signature-nvim" },
   { import = "astrocommunity.lsp.lsplinks-nvim" },
   { import = "astrocommunity.lsp.nvim-lsp-endhints" },
+  -- motion
+  { import = "astrocommunity.motion.flash-nvim" },
+  { import = "astrocommunity.motion.nvim-surround" },
+  { import = "astrocommunity.motion.nvim-spider" },
+  { import = "astrocommunity.motion.mini-bracketed" },
+  { import = "astrocommunity.motion.mini-move" },
+  { import = "astrocommunity.motion.nvim-tree-pairs" },
+  -- { import = "astrocommunity.motion.vim-matchup" },
   -- markdown & latex
   { import = "astrocommunity.markdown-and-latex.render-markdown-nvim" },
   -- neovim-lua-development
@@ -52,12 +70,20 @@ return {
   { import = "astrocommunity.quickfix.quicker-nvim" },
   -- recipes
   { import = "astrocommunity.recipes.astrolsp-no-insert-inlay-hints" },
+  { import = "astrocommunity.recipes.telescope-lsp-mappings" },
   { import = "astrocommunity.recipes.neovide" },
   -- scrolling
-  { import = "astrocommunity.scrolling.neoscroll-nvim" },
+  { import = "astrocommunity.scrolling.satellite-nvim" },
+  -- { import = "astrocommunity.scrolling.cinnamon-nvim" },
+  -- syntax
+  { import = "astrocommunity.syntax.vim-easy-align" },
   -- search
   { import = "astrocommunity.search.grug-far-nvim" },
   -- utility
   { import = "astrocommunity.utility.noice-nvim" },
+  -- workflow
+  -- { import = "astrocommunity.workflow.bad-practices-nvim" },
+  { import = "astrocommunity.workflow.hardtime-nvim" },
+  { import = "astrocommunity.workflow.precognition-nvim" },
   -- import/override with your plugins folder
 }
